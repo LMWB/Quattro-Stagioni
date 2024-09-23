d4d5e95f06146034ff71dee1f1fab1d05c46802c
MaxWilli86 <lukas.bartels@smartcube360.de>
2024-09-23 14:37:11 +0200

    smoothing measured data improves plots

--- Testing/results_pt1000_bridge.m
+++ Testing/results_pt1000_bridge.m
@@ -12,12 +12,21 @@ temperature_ds18b20_cold = data2([1:end], 4);
 temperature_pt1000_warm = data1([1:end], 11);
 temperature_ds18b20_warm = data2([1:end], 7);
 
+%%
+% Die Daten werden gemittelt bzw. geglaettet um eine besseren Analyse zu
+N = 8;                                                                      % Gleitender Mittelwert f. Daten Analyse
+h_mean  = ones(1,N)*1/N;
+temperature_pt1000_cold_lp = filter(h_mean, 1, temperature_pt1000_cold);
+temperature_pt1000_warm_lp = filter(h_mean, 1, temperature_pt1000_warm);
+
 figure()
 hold on
-plot(temperature_pt1000_cold)
+#plot(temperature_pt1000_cold)
+plot(temperature_pt1000_cold_lp)
 plot(temperature_ds18b20_cold)
 
 figure()
 hold on
-plot(temperature_pt1000_warm)
+#plot(temperature_pt1000_warm)
+plot(temperature_pt1000_warm_lp)
 plot(temperature_ds18b20_warm)
