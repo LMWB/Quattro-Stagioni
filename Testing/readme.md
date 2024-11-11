224027289711a3d9ca98e4e7ab069cf27b59e530
MaxWilli86 <lukas.bartels@smartcube360.de>
2024-11-11 20:32:48 +0100

    added images and text to results-readme

--- Testing/readme.md
+++ Testing/readme.md
@@ -7,9 +7,25 @@ So I use the results of the DS18B20 to estimate how good my measurement bridge i
 
 I than use a rubber band to attache one DS18B20 to one PT1000 sensor. I did this twice. On pair is going to do a *hot to room temperature test*. The other pair is doing a *cold to room temperature test*. I use a metal small pitcher filled with hot water from the kettle. And I use thermal pack from the freezer. Both gets equipped with one pair of temperature sensors.
 
-To start the testing scene I only had to plug in the two Nucleo boards to my computer and start a serial terminal to log the outputs to console. I used CuteCom. The firmware of each Nucleo board prints the temperature conversion results to serial monitor with 500ms period (f=2Hz). Total record time is T = N/f = 15540/2Hz = 7770 sec = 2h:10min.
+## Execution
+To start the testing scene I only had to plug in the two Nucleo boards to my computer and start a serial terminal to log the outputs to console. I used CuteCom. The firmware of each Nucleo board prints the temperature conversion results to serial monitor with 5000ms period (f=0.2Hz). Total record time is T = N/f = 15540/0.2Hz = 77700 sec = 21h:30min.
+
+## Interpretation
+Study the plots two things are to mention: 
+- the measurement bridge (blue line) is much noisy 
+- red line and blue line are quit identical except below zero degrees Celsius
+
+The glitch in the cold record at around 3800 ms is due to my impatience. I lifted the thermal pack to feel the cold with my hand. At least I could verify that both type of sensors a quick in response :).
 
 ![](../Images/results-cold-to-room.png)
 ![](../Images/results-hot-to-room.png)
 ![](../Images/results-cold-to-room-lp.png)
 ![](../Images/results-hot-to-room-lp.png)
+
+- to smooth the measurements I added a moving-average filter
+- recap the schematics and the simulation done there it is clear, that this circuit is not gonna work for temperatures below zero, so the behavior is quit as expected.
+
+## Error
+![](../Images/results-error.png)
+
+## Conclusions
--- Testing/results_pt1000_bridge.m
+++ Testing/results_pt1000_bridge.m
@@ -4,13 +4,18 @@ filename2 = "ds18b20-log.csv"
 data1 = dlmread (filename1, ',');
 data2 = dlmread (filename2, ',');
 
-tick1 = data1([1:end], 2);
-tick2 = data2([1:end], 2);
+%%
+% convert ms-ticks to hours
+tick1 = data1([1:end], 2) / 1000 / 60 / 60; 
+tick2 = data2([1:end], 2) / 1000 / 60 / 60 ;
 
-temperature_pt1000_cold = data1([1:end], 6);
-temperature_ds18b20_cold = data2([1:end], 4);
-temperature_pt1000_warm = data1([1:end], 11);
-temperature_ds18b20_warm = data2([1:end], 7);
+%%
+% Daten sind in Grad Celsius * 1000 (Festkomma)
+temperature_pt1000_cold = data1([1:end], 6) / 1000; 
+temperature_pt1000_warm = data1([1:end], 11) / 1000;
+
+temperature_ds18b20_cold = data2([1:end], 4) / 1000;
+temperature_ds18b20_warm = data2([1:end], 7) / 1000;
 
 %%
 % Die Daten werden gemittelt bzw. geglaettet um eine besseren Analyse zu
@@ -21,12 +26,38 @@ temperature_pt1000_warm_lp = filter(h_mean, 1, temperature_pt1000_warm);
 
 figure()
 hold on
-#plot(temperature_pt1000_cold)
-plot(temperature_pt1000_cold_lp)
-plot(temperature_ds18b20_cold)
+#plot(tick1, temperature_pt1000_cold)
+plot(tick1, temperature_pt1000_cold_lp)
+plot(tick2, temperature_ds18b20_cold)
+title(['from cold to room temperature'])
+ylabel('T / °C')
+xlabel('t / h')
+legend('PT1000',  'DS18B20', 'Location', 'NorthWest')
+
 
 figure()
 hold on
-#plot(temperature_pt1000_warm)
-plot(temperature_pt1000_warm_lp)
-plot(temperature_ds18b20_warm)
+#plot(tick1, temperature_pt1000_warm)
+plot(tick1, temperature_pt1000_warm_lp)
+plot(tick2, temperature_ds18b20_warm)
+title(['from hot to room temperature'])
+ylabel('T / °C')
+xlabel('t / h')
+legend('PT1000',  'DS18B20', 'Location', 'NorthWest')
+
+%%
+% Error, cold-series has less data point, so its need to take its len as reference to do calculations
+len = length(temperature_pt1000_cold_lp)
+err_pt1000_cold = abs(temperature_pt1000_cold_lp([N:end]) - temperature_ds18b20_cold([N:len])) ./ temperature_ds18b20_cold([N:len]);
+err_pt1000_warm = abs(temperature_pt1000_warm_lp([N:end]) - temperature_ds18b20_warm([N:len])) ./ temperature_ds18b20_warm([N:len]);
+
+figure()
+hold on
+plot(tick1([N:end]), err_pt1000_cold*100)
+plot(tick1([N:end]), err_pt1000_warm*100)
+ylim([-100,100])
+title(['error'])
+ylabel('error / %')
+xlabel('t / h')
+legend('cold',  'warm', 'Location', 'NorthWest')
+
